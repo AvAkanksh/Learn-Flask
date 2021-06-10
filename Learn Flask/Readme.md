1fa6ca444802c50dabfbb379c7ab9bc710af6e14
Akanksh <akanksh@localhost.localdomain>
2021-06-11 04:32:26 +0530

    updating the files

--- Learn Flask/Readme.md
+++
@@ -1,9 +0,0 @@
-# Learning Flask for Data Science
-
-following Youtube playlist [Flask Tutorial](https://www.youtube.com/playlist?list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH)
-
-Total 15 Videos
-## Day 1
-- [ ] Video 1
-
-
--- flaskblog.py
+++ flaskblog.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 @app.route('/home')
 @app.route('/')
 def hello():
-    return '<h1>Hello text</h1>'
+    return '<h1>Hello hi text</h1>'
 
 @app.route('/about')
 def ab():
-    return '<h1>About Page</h1>'
+    return '<h1>About hello Page</h1>'
 
 
 if(__name__ == '__main__'):
