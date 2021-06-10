d391f8497362043d7c628dbc961bf478d592e72b
Akanksh <akanksh@localhost.localdomain>
2021-06-11 03:33:44 +0530

    updating the files

--- flaskblog.py
+++ flaskblog.py
@@ -2,15 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route('/home')
+@app.route('/')
 def hello():
     return '<h1>Hello Guy!</h1>'
 
 @app.route('/about')
-
 def ab():
     return '<h1>About Page</h1>'
+
 
 if(__name__ == '__main__'):
     app.run(debug=True)
