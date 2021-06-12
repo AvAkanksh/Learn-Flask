0a61107db72611ed1bc52359b442490a1c011625
Akanksh <akanksh@localhost.localdomain>
2021-06-12 06:36:34 +0530

    updating the files

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,32 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
 
+posts = [
+    {
+        'author'    : 'Akanksh',
+        'title'     : 'Blog Post 1',
+        'content'   : 'First post content',
+        'data_posted': ' June 11 , 2021'
+    },
+    {
+        'author'    : 'Akshitha',
+        'title'     : 'Blog Post 2',
+        'content'   : 'Second post content',
+        'data_posted': ' June 10 , 2021'
+    }
+]
+
+
 @app.route('/home')
 @app.route('/')
-def hello():
-    return '<h1>Hello hi text</h1>'
+def home():
+    return render_template('home.html', p = posts, title='Home')
 
 @app.route('/about')
-def ab():
-    return '<h1>About Page</h1>'
+def about():
+    return render_template('about.html', title='About')
 
 
 if(__name__ == '__main__'):
